b48634d84ef6a68f05f6a61761fd96135fbc7635
uglovevgenij <uglovevgenij@gmail.com>
2020-05-24 20:11:35 +0300

    hw

---
+++ Lesson_4/HW1.py
@@ -0,0 +1,15 @@
+"""Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника.
+В расчете необходимо использовать формулу: (выработка в часах*ставка в час) + премия. Для выполнения расчета для
+конкретных значений необходимо запускать скрипт с параметрами."""
+
+from sys import argv
+
+script_name, first_param, second_param, third_param = argv
+
+print("Имя скрипта: ", script_name)
+print("Выработка в часах: ", first_param)
+print("Ставка в час: ", second_param)
+print("Премия: ", third_param)
+print("Итог: ", int(first_param) * int(second_param) + int(third_param))
+
+#
